6c5c4d2ec3bacd5caa6983a361e50d5eca941a86
austin <austinchang4@gmail.com>
2022-04-20 10:28:50 +0800

    refactor: move route53 dns record into same file as deployment

--- k8s/dns.tf
+++ k8s/dns.tf
@@ -1,13 +1,3 @@
 resource "aws_route53_zone" "downme" {
   name = "downme.xyz"
 }
-
-resource "aws_route53_record" "searx" {
-  name    = "searx.downme.xyz"
-  type    = "A"
-  ttl     = "300"
-  zone_id = aws_route53_zone.downme.zone_id
-  records = [
-    scaleway_lb_ip.nginx_ip.ip_address,
-  ]
-}
--- k8s/searx.tf
+++ k8s/searx.tf
@@ -104,3 +104,15 @@ resource "kubernetes_ingress_v1" "searx-ingress" {
     }
   }
 }
+
+
+
+resource "aws_route53_record" "searx" {
+  name    = "searx.downme.xyz"
+  type    = "A"
+  ttl     = "300"
+  zone_id = aws_route53_zone.downme.zone_id
+  records = [
+    scaleway_lb_ip.nginx_ip.ip_address,
+  ]
+}
