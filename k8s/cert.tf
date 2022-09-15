5bde7006dd580c6dee35cacf50c2d86c6a1c5a5c
austin <austinchang4@gmail.com>
2022-09-15 23:03:26 +0800

    chore: update cert-manager helm version

--- k8s/cert.tf
+++ k8s/cert.tf
@@ -15,6 +15,7 @@ resource "helm_release" "cert-manager" {
   namespace  = "cert-manager"
   repository = "https://charts.jetstack.io"
   chart      = "cert-manager"
+  version    = "1.9.1"
   set {
     name  = "installCRDs"
     value = true
